cf43e67e0dbffde62dccdfceb0350f474e86e752
Antonio Molinari <molinari@incode.it>
2023-04-07 23:59:17 +0200

    first steps

--- bot.js
+++ bot.js
@@ -1,5 +1,37 @@
+import dotenv from 'dotenv';
 import ccxt from 'ccxt';
+import dayjs from 'dayjs';
 
-const exchange = new ccxt.coinbasepro({});
-const ticker = await exchange.fetchTicker('BTC/USD');
-console.log(ticker.last);
+dotenv.config();
+class Bot {
+  constructor(exchange) {
+    this.exchange = exchange;
+  }
+
+  async price_in_usd(currency) {
+    const ticker = await this.exchange.fetchTicker(currency + '/USD');
+    return ticker.last;
+  }
+
+  async value_in_usd(value, currency) {
+    const price_in_usd = await this.price_in_usd(currency);
+    return value / price_in_usd;
+  }
+
+
+  async run() {
+    const amount = 100;
+    const currency = 'ETH'
+    const value = await this.value_in_usd(amount, currency);
+    console.log(`${amount} $ is worth ${value.toFixed(2)} ${currency} at ${dayjs()}`);
+  }
+}
+
+
+const exchange = new ccxt.coinbase({
+  apiKey: process.env.COINBASE_API_KEY,
+  secret: process.env.COINBASE_SECRET,
+  enableRateLimit: true,
+});
+const bot = new Bot(exchange);
+bot.run();
